b80a85a092e66e749aa0dbb6021745e2123ad907
Md.Sujan Ahmed <csesujon155@gmail.com>
2021-05-13 01:28:06 +0600

    Projects with magnificpopup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,40 @@
-$(function ($){
+$(function ($) {
     'use strict'
 
     //Data--Background
-    $('[data-background]').each(function(){
-        $(this).css('background-image', 'url('+ $(this).attr('data-background') + ')');
+    $('[data-background]').each(function () {
+        $(this).css('background-image', 'url(' + $(this).attr('data-background') + ')');
     });
 
     //Owl-Carousel (For Single Slider)
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
+        loop: true,
+        margin: 10,
         autoplay: true,
         autoplayHoverPause: true,
         autoplaySpeed: 1000,
         dots: true,
         loop: true,
-        nav:false,
-        responsive:{
-            0:{
-                items:1
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:1
+            600: {
+                items: 1
             },
-            1000:{
-                items:1
+            1000: {
+                items: 1
             }
         }
     })
+
+    //Project - MagnificPopup Image 
+    $('.project-popup').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
 
 })(jQuery)
