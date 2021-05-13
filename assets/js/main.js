3248a06e22381b545b93f11b8dc945663633b2f0
Md.Sujan Ahmed <csesujon155@gmail.com>
2021-05-13 21:55:11 +0600

    CSS Section Name and decoration

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,13 +113,13 @@ $(function ($) {
         filter: '',
         easing: 'swing',
         begin: function() {
-            //I get fired when the animation is starting
+           
         },
         end: function() {
-            //I get fired when the animation is ending
+           
         },
         scrollChange: function($currentListItem) {
-            //I get fired when you enter a section and I pass the list item of the section
+           
         }
     });
 
