87ea353f7fa676828e4fea00aee047f01b894290
Md.Sujan Ahmed <csesujon155@gmail.com>
2021-05-13 20:55:31 +0600

    OnePage Nav Apply for Smooth Scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,7 @@ $(function ($) {
     $('.popup-video').magnificPopup({
         type: 'iframe'
     });
-    
+
     //Owl-Carousel (For Testimonials Area)
     $('.testimonials').owlCarousel({
         loop: true,
@@ -94,4 +94,36 @@ $(function ($) {
         }
     })
 
+    //ScrollTop (For Menu Fixed)
+    $(window).on('scroll',function (){
+        if ($(this).scrollTop() > 1 ) {
+            $('.header-top').addClass('sticky');
+        } else {
+            $('.header-top').removeClass('sticky');
+        }
+    });
+
+
+    //One Page Nav for Smooth Scrolling 
+    $('#nav').onePageNav({
+        currentClass: 'current',
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'swing',
+        begin: function() {
+            //I get fired when the animation is starting
+        },
+        end: function() {
+            //I get fired when the animation is ending
+        },
+        scrollChange: function($currentListItem) {
+            //I get fired when you enter a section and I pass the list item of the section
+        }
+    });
+
 })(jQuery)
+
+
+
